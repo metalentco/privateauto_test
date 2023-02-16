99dd8d896122a8e7c2e9a901cd46b487553f685e
metalentco <metalentco777@gmail.com>
2023-02-16 09:41:24 -0500

    ContentBlock component is updated

--- components/ContentBlock.tsx
+++ components/ContentBlock.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import { twMerge } from "tailwind-merge";
+import ContentButtons from "@/components/subComponent/ContentButtons";
 import { parseColor, parseTitle } from "@/libs/utils";
 
 type Props = {
@@ -32,57 +33,7 @@ const ContentBlock = ({ data }: Props) => {
         >
           <div dangerouslySetInnerHTML={{ __html: data.Content }}></div>
         </div>
-        {data.Buttons == "Sell & Browse" ? (
-          <div className="flex space-x-8 pt-12">
-            <button className=" bg-[#00b3de] hover:bg-blue-300 text-white text-base font-normal py-2 px-3 rounded-sm cursor-pointer">
-              Sell a vehicle
-            </button>
-            <button className=" bg-[#eff0f6] hover:bg-slate-300 text-black text-base font-bonormalld py-2 px-3 rounded-sm cursor-pointer">
-              Browse Vehicles <strong>&#62;</strong>
-            </button>
-          </div>
-        ) : data.Buttons == "Sell" ? (
-          <div className="pt-12">
-            <button className=" bg-[#00b3de] hover:bg-blue-300 text-white text-base font-normal py-2 px-3 rounded-sm cursor-pointer">
-              Sell your vehicle
-            </button>
-          </div>
-        ) : data.Buttons == "Browse" ? (
-          <div className="pt-12">
-            <button className=" bg-[#00b3de] hover:bg-blue-300 text-white text-base font-normal py-2 px-3 rounded-sm cursor-pointer">
-              Browse Vehicles
-            </button>
-          </div>
-        ) : data.Buttons == "App Stores" ? (
-          <div className="flex justify-center sm:justify-start mt-8 space-x-8">
-            <a
-              href="https://apps.apple.com/us/app/privateauto-sell-privately/id1614271597"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <Image
-                width={132}
-                height={45}
-                src="/assets/appstore.svg"
-                alt="appstore"
-              />
-            </a>
-            <a
-              href="https://play.google.com/store/apps/details?id=io.gonative.android.qwkoyk"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <Image
-                width={137}
-                height={49}
-                src="/assets/googleplay.svg"
-                alt="googleplay"
-              />
-            </a>
-          </div>
-        ) : (
-          ""
-        )}
+        <ContentButtons buttonType={data.Buttons} />
       </div>
       <div className="w-full sm:w-1/2 flex md:block justify-center">
         {data.Image.data != null ? (
@@ -135,57 +86,7 @@ const ContentBlock = ({ data }: Props) => {
           >
             <div dangerouslySetInnerHTML={{ __html: data.Content }}></div>
           </div>
-          {data.Buttons == "Sell & Browse" ? (
-            <div className="flex space-x-8 pt-12">
-              <button className=" bg-[#00b3de] hover:bg-blue-300 text-white text-base font-normal py-2 px-3 rounded-sm cursor-pointer">
-                Sell a vehicle
-              </button>
-              <button className=" bg-[#eff0f6] hover:bg-slate-300 text-black text-base font-bonormalld py-2 px-3 rounded-sm cursor-pointer">
-                Browse Vehicles <strong>&#62;</strong>
-              </button>
-            </div>
-          ) : data.Buttons == "Sell" ? (
-            <div className="pt-12">
-              <button className=" bg-[#00b3de] hover:bg-blue-300 text-white text-base font-normal py-2 px-3 rounded-sm cursor-pointer">
-                Sell your vehicle
-              </button>
-            </div>
-          ) : data.Buttons == "Browse" ? (
-            <div className="pt-12">
-              <button className=" bg-[#00b3de] hover:bg-blue-300 text-white text-base font-normal py-2 px-3 rounded-sm cursor-pointer">
-                Browse Vehicles
-              </button>
-            </div>
-          ) : data.Buttons == "App Stores" ? (
-            <div className="flex justify-center sm:justify-start mt-8 space-x-8">
-              <a
-                href="https://apps.apple.com/us/app/privateauto-sell-privately/id1614271597"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <Image
-                  width={132}
-                  height={45}
-                  src="/assets/appstore.svg"
-                  alt="appstore"
-                />
-              </a>
-              <a
-                href="https://play.google.com/store/apps/details?id=io.gonative.android.qwkoyk"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <Image
-                  width={137}
-                  height={49}
-                  src="/assets/googleplay.svg"
-                  alt="googleplay"
-                />
-              </a>
-            </div>
-          ) : (
-            ""
-          )}
+          <ContentButtons buttonType={data.Buttons} />
         </div>
       </div>
     </div>
@@ -222,57 +123,7 @@ const ContentBlock = ({ data }: Props) => {
           >
             <div dangerouslySetInnerHTML={{ __html: data.Content }}></div>
           </div>
-          {data.Buttons == "Sell & Browse" ? (
-            <div className="flex space-x-8 pt-12">
-              <button className=" bg-[#00b3de] hover:bg-blue-300 text-white text-base font-normal py-2 px-3 rounded-sm cursor-pointer">
-                Sell a vehicle
-              </button>
-              <button className=" bg-[#eff0f6] hover:bg-slate-300 text-black text-base font-bonormalld py-2 px-3 rounded-sm cursor-pointer">
-                Browse Vehicles <strong>&#62;</strong>
-              </button>
-            </div>
-          ) : data.Buttons == "Sell" ? (
-            <div className="pt-12">
-              <button className=" bg-[#00b3de] hover:bg-blue-300 text-white text-base font-normal py-2 px-3 rounded-sm cursor-pointer">
-                Sell your vehicle
-              </button>
-            </div>
-          ) : data.Buttons == "Browse" ? (
-            <div className="pt-12">
-              <button className=" bg-[#00b3de] hover:bg-blue-300 text-white text-base font-normal py-2 px-3 rounded-sm cursor-pointer">
-                Browse Vehicles
-              </button>
-            </div>
-          ) : data.Buttons == "App Stores" ? (
-            <div className="flex justify-center sm:justify-start mt-8 space-x-8">
-              <a
-                href="https://apps.apple.com/us/app/privateauto-sell-privately/id1614271597"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <Image
-                  width={132}
-                  height={45}
-                  src="/assets/appstore.svg"
-                  alt="appstore"
-                />
-              </a>
-              <a
-                href="https://play.google.com/store/apps/details?id=io.gonative.android.qwkoyk"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <Image
-                  width={137}
-                  height={49}
-                  src="/assets/googleplay.svg"
-                  alt="googleplay"
-                />
-              </a>
-            </div>
-          ) : (
-            ""
-          )}
+          <ContentButtons buttonType={data.Buttons} />
         </div>
         <div className="w-full md:w-1/3 flex md:block justify-center">
           <Image
---
+++ components/subComponent/ContentButtons.tsx
@@ -0,0 +1,65 @@
+import Image from "next/image";
+
+type Props = {
+  buttonType: string;
+};
+
+const ContentButtons = ({ buttonType }: Props) => {
+  return (
+    <div>
+      {buttonType == "Sell & Browse" ? (
+        <div className="flex space-x-8 pt-12">
+          <button className=" bg-[#00b3de] hover:bg-blue-300 text-white text-base font-normal py-2 px-3 rounded-sm cursor-pointer">
+            Sell a vehicle
+          </button>
+          <button className=" bg-[#eff0f6] hover:bg-slate-300 text-black text-base font-bonormalld py-2 px-3 rounded-sm cursor-pointer">
+            Browse Vehicles <strong>&#62;</strong>
+          </button>
+        </div>
+      ) : buttonType == "Sell" ? (
+        <div className="pt-12">
+          <button className=" bg-[#00b3de] hover:bg-blue-300 text-white text-base font-normal py-2 px-3 rounded-sm cursor-pointer">
+            Sell your vehicle
+          </button>
+        </div>
+      ) : buttonType == "Browse" ? (
+        <div className="pt-12">
+          <button className=" bg-[#00b3de] hover:bg-blue-300 text-white text-base font-normal py-2 px-3 rounded-sm cursor-pointer">
+            Browse Vehicles
+          </button>
+        </div>
+      ) : buttonType == "App Stores" ? (
+        <div className="flex justify-center sm:justify-start mt-8 space-x-8">
+          <a
+            href="https://apps.apple.com/us/app/privateauto-sell-privately/id1614271597"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <Image
+              width={132}
+              height={45}
+              src="/assets/appstore.svg"
+              alt="appstore"
+            />
+          </a>
+          <a
+            href="https://play.google.com/store/apps/details?id=io.gonative.android.qwkoyk"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <Image
+              width={137}
+              height={49}
+              src="/assets/googleplay.svg"
+              alt="googleplay"
+            />
+          </a>
+        </div>
+      ) : (
+        ""
+      )}
+    </div>
+  );
+};
+
+export default ContentButtons;
